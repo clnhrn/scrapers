6c5c281f5ce9375f4f50e983671160944adc0492
clnhrn <herniacln@gmail.com>
2022-09-14 22:57:48 +0800

    tsp details configured as dictionary within list

--- williams.py
+++ williams.py
@@ -12,29 +12,40 @@ logger = logging.getLogger(__name__)
 
 
 class Williams(PipelineScraper):
-    tsp = None
-    tsp_name = None
-    source_extensions = ['discovery', 'blackmarlin']
-    source = 'williams'
-    base_url = 'https://{}.williams.com'
-    post_url = 'https://{}.williams.com/oa_detail.jsp'
-    download_csv_url = 'https://{}.williams.com/oa_detail.jsp'
+    parent_source = 'williams'
+    tsp_list = [
+        {
+            'source': 'discovery.williams',
+            'home_url': 'https://discovery.williams.com',
+            'download_api': 'https://discovery.williams.com/oa_detail.jsp',
+            # can't find tsp id
+            'tsp_id': '',
+            'tsp_name': 'Discovery Gas Transmission LLC'
+        },
+        {
+            'source': 'blackmarlin.williams',
+            'home_url': 'https://blackmarlin.williams.com',
+            'download_api': 'https://blackmarlin.williams.com/oa_detail.jsp',
+            # can't find tsp id
+            'tsp_id': '',
+            'tsp_name': 'Black Marlin Pipeline Company'
+        }]
 
     def __init__(self, job_id=None):
-        PipelineScraper.__init__(self, job_id, web_url=self.base_url, source=self.source)
+        PipelineScraper.__init__(self, job_id, web_url=self.tsp_list[0]['home_url'], source=self.parent_source)
 
     def start_scraping(self, post_date: date = None, cycle: int = 2):
         post_date = post_date if post_date is not None else date.today()
 
         main_df = pd.DataFrame()
-        for extension in self.source_extensions:
+        for tsp in self.tsp_list:
             try:
                 query_params = [
                     ('id', cycle),
                     ('nomDate', post_date.strftime('%m-%d-%Y'))
                 ]
-                logger.info('Scraping %s pipeline gas for post date: %s', self.source, post_date)
-                response = self.session.get(self.post_url.format(extension), params=query_params)
+                logger.info('Scraping %s pipeline gas for post date: %s', tsp['source'], post_date)
+                response = self.session.get(tsp['download_api'], params=query_params)
                 response.raise_for_status()
 
                 soup = BeautifulSoup(response.content, 'lxml')
@@ -42,15 +53,14 @@ class Williams(PipelineScraper):
                 table = soup.select_one('table.sortable')
                 df_result = pd.read_html(str(table))[0]
 
-                tsp_name = soup.select_one('title').text
                 effective_date = soup.find_all("b", string="Effective Date:")[0].findNext("td").text
                 posting_date = soup.find_all("b", string="Posting Date:")[0].findNext("td").text
 
-                # couldn't find TSP ID on the website.
-                df_result.insert(0, 'TSP Name', tsp_name, allow_duplicates=True)
+                df_result.insert(0, 'TSP Name', tsp['tsp_name'], allow_duplicates=True)
                 df_result.insert(1, 'Post Date/Time', posting_date, allow_duplicates=True)
                 df_result.insert(2, 'Effective Gas Day/Time', effective_date, allow_duplicates=True)
-                df_result.insert(3, 'Meas Basis Desc', 'MMBTU')  # All values are MMBTU mentioned static on website.
+                # All values are MMBTU mentioned static on website.
+                df_result.insert(3, 'Meas Basis Desc', 'MMBTU')
 
                 main_df = pd.concat([main_df, df_result])
             except Exception as ex:
@@ -71,7 +81,7 @@ def back_fill_pipeline_date():
 
 
 def main():
-    query_date = datetime.fromisoformat("2022-08-11")
+    query_date = datetime.fromisoformat("2022-09-13")
     scraper = Williams(job_id=str(uuid.uuid4()))
 
     # cycle = 1 - Timely
@@ -80,7 +90,7 @@ def main():
     # cycle = 4 - Intraday 2
     # cycle = 5 - Intraday 3
 
-    scraper.start_scraping(post_date=query_date, cycle=2)
+    scraper.start_scraping(post_date=query_date, cycle=5)
     scraper.scraper_info()
 
 
