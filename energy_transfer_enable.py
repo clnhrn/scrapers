0a141cf33c9892c370f6138b1b21a4a68fb6b26e
clnhrn <herniacln@gmail.com>
2022-09-16 16:59:01 +0800

    tsp details/urls configured as dictionary within list

--- energy_transfer_enable.py
+++ energy_transfer_enable.py
@@ -13,13 +13,25 @@ logger = logging.getLogger(__name__)
 
 
 class EnergyTransferEnable(PipelineScraper):
-    tsp = ['872670161', '006968077']
-    tsp_name = ['ENABLE GAS TRANSMISSION, LLC', 'ENABLE MISSISSIPPI RIVER TRANSMISSION, LLC']
-    asset = ['EGT', 'MRT']
-    source = 'pipelines.energytransfer'
-    api_url = 'https://pipelines.energytransfer.com'
-
-    get_url = 'https://pipelines.energytransfer.com/ipost/{}/capacity/enbl-operationally-available?max=ALL'
+    parent_source = 'energytransfer'
+    tsp_list = [
+        {
+            'source': 'pipelines.energytransfer',
+            'home_url': 'https://pipelines.energytransfer.com/ipost/EGT/main/index',
+            'api_url': 'https://pipelines.energytransfer.com',
+            'get_url': 'https://pipelines.energytransfer.com/ipost/EGT/capacity/enbl-operationally-available?max=ALL',
+            'tsp_id': '872670161',
+            'tsp_name': 'ENABLE GAS TRANSMISSION, LLC'
+        },
+        {
+            'source': 'pipelines.energytransfer',
+            'home_url': 'https://pipelines.energytransfer.com/ipost/MRT/main/index',
+            'api_url': 'https://pipelines.energytransfer.com',
+            'get_url': 'https://pipelines.energytransfer.com/ipost/MRT/capacity/enbl-operationally-available?max=ALL',
+            'tsp_id': '006968077',
+            'tsp_name': 'ENABLE MISSISSIPPI RIVER TRANSMISSION, LLC'
+        }
+    ]
 
     get_page_headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -39,19 +51,20 @@ class EnergyTransferEnable(PipelineScraper):
         'sec-ch-ua-platform': '"Windows"'
           }
 
-    cycle_choices = {0: 'TIMELY',
-                     1: 'EVENING',
-                     2: 'INTRADAY 1',
-                     3: 'INTRADAY 2',
-                     4: 'INTRADAY 3',
-                     5: 'FINAL'}
+    cycle_options = {
+        0: 'TIMELY',
+        1: 'EVENING',
+        2: 'INTRADAY 1',
+        3: 'INTRADAY 2',
+        4: 'INTRADAY 3',
+        5: 'FINAL'
+    }
 
     def __init__(self, job_id):
-        PipelineScraper.__init__(self, job_id, web_url=self.api_url, source=self.source)
+        PipelineScraper.__init__(self, job_id, web_url=self.tsp_list[0]['api_url'], source=self.parent_source)
 
-    def get_report_list(self, sub_comp):
-        asset_report_url = self.get_url.format(sub_comp)
-        response = self.session.get(asset_report_url, headers=self.get_page_headers)
+    def get_report_list(self, url):
+        response = self.session.get(url, headers=self.get_page_headers, timeout=120)
         return response.text
 
     def get_addtl_info(self, df_data):
@@ -73,9 +86,9 @@ class EnergyTransferEnable(PipelineScraper):
 
         return info_dict
 
-    def add_columns(self, num, report, details):
-        report.insert(0, 'TSP', self.tsp[num], True)
-        report.insert(1, 'TSP Name', self.tsp_name[num], True)
+    def add_columns(self, tsp, report, details):
+        report.insert(0, 'TSP', tsp['tsp_id'], True)
+        report.insert(1, 'TSP Name', tsp['tsp_name'], True)
         report.insert(2, 'Post Date/Time', details['Posting Date/Posting Time'], True)
         report.insert(3, 'Effective Gas Day/Time', details['Effective Gas Day/Effective Time'], True)
         report.insert(4, 'Cycle', details['Cycle'], True)
@@ -84,15 +97,15 @@ class EnergyTransferEnable(PipelineScraper):
 
         return report
 
-    def start_scraping(self, cycle: int = None, post_date: date = None):
+    def start_scraping(self, post_date: date = None, cycle: int = 0):
         post_date = post_date if post_date is not None else date.today().strftime('%Y-%m-%d')
-        cycle = cycle if cycle is not None else 5
-        matches = [post_date.strftime('%Y-%m-%d'), self.cycle_choices[cycle]]
+        matches = [post_date.strftime('%Y-%m-%d'), self.cycle_options[cycle]]
+
         main_df = pd.DataFrame()
-        for index, asset in enumerate(self.asset):
+        for tsp in self.tsp_list:
             try:
-                logger.info('Scraping %s pipeline gas for post date: %s', self.source, post_date)
-                reports = self.get_report_list(asset)
+                logger.info('Scraping %s pipeline gas for post date: %s', tsp['source'], post_date)
+                reports = self.get_report_list(tsp['get_url'])
                 soup = BeautifulSoup(reports, 'lxml')
                 user_report = soup.find_all('td', class_='reportTitle text-center')
                 for rep in user_report:
@@ -101,9 +114,11 @@ class EnergyTransferEnable(PipelineScraper):
                     if all(words in report_cell for words in matches):
                         view_report_elem = rep.parent.findChild('td', class_='id').findChild(href=True)
                         view_report_link = view_report_elem['href']
-                        csv_url = self.api_url + view_report_link
+                        csv_url = tsp['api_url'] + view_report_link
+
                         page_response = self.session.get(csv_url, headers=self.get_page_headers)
                         page_response.raise_for_status()
+
                         html_text = page_response.text
                         soup = BeautifulSoup(html_text, 'lxml')
                         pre = soup.select_one('pre').text
@@ -121,12 +136,13 @@ class EnergyTransferEnable(PipelineScraper):
                                 rows.append(row)
                             else:
                                 break
+
                         rows.pop()
                         df = pd.DataFrame(rows, columns=columns)
-                        rep_details = self.get_addtl_info(pre)
-                        final_report = self.add_columns(index, df, rep_details)
+                        report_details = self.get_addtl_info(pre)
+                        final_report = self.add_columns(tsp, df, report_details)
                         main_df = pd.concat([main_df, final_report])
-                        logger.info('DF created for %s', asset)
+                        logger.info('DF created for %s', tsp['source'])
                     else:
                         continue
 
@@ -134,7 +150,7 @@ class EnergyTransferEnable(PipelineScraper):
                 logger.error(ex, exc_info=True)
 
         self.save_result(main_df, post_date=post_date, local_file=True)
-        logger.info('File saved. end of scraping: %s', self.source)
+        logger.info('File saved. end of scraping: %s', self.parent_source)
 
         return None
 
@@ -149,12 +165,16 @@ def back_fill_pipeline_date():
 
 def main():
     # set your own date to scrape. default is current date.
-    custom_date = date.fromisoformat('2022-09-09')
-    # set desired cycle: 0 = timely, 1 = evening, 2 = ID1, 3 = ID2, 4 = ID3, 5 = Final
-    # default value is 5 = final; csv file might be empty if cycle = 5 is not yet available.
-    custom_cycle = 3
+    custom_date = date.fromisoformat('2022-09-11')
+    # set desired cycle:
+    # 0 = Timely (Default),
+    # 1 = Evening,
+    # 2 = ID1,
+    # 3 = ID2,
+    # 4 = ID3,
+    # 5 = Final - csv file might be empty if not yet available
+    custom_cycle = 2
     scraper = EnergyTransferEnable(job_id=str(uuid.uuid4()))
-    # parameters are cycle, post_date
     scraper.start_scraping(cycle=custom_cycle, post_date=custom_date)
     scraper.scraper_info()
 
